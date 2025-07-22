539b73c2b811e5a2b0c01f4630f59b279baf472a
yqy <dwadaw@dae>
2025-07-22 16:10:57 +0800

    [会话记忆Advisor] 使每次对话时模型拥有上下文

--- src/main/java/com/yu/springai/config/CommonConfiguration.java
+++ src/main/java/com/yu/springai/config/CommonConfiguration.java
@@ -1,7 +1,10 @@
 package com.yu.springai.config;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.InMemoryChatMemoryRepository;
 import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,11 +12,15 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class CommonConfiguration {
     @Bean
-    public ChatClient chatClient(OpenAiChatModel model){
+    public ChatClient chatClient(OpenAiChatModel model, ChatMemory chatMemory){
         return ChatClient
                 .builder(model)
                 .defaultSystem("你是一位医生") // 系统提示词
-                .defaultAdvisors(new SimpleLoggerAdvisor()) // 环绕通知，比如日志
+                .defaultAdvisors(
+                        new SimpleLoggerAdvisor(),
+                        MessageChatMemoryAdvisor.builder(chatMemory)
+                                .build()
+                ) // 环绕通知，比如日志
                 .build();
 
     }
