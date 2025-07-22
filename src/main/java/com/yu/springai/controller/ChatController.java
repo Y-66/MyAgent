24a7bd34c34c0b23f5ca50da749edf530f864d7f
yqy <dwadaw@dae>
2025-07-22 16:11:37 +0800

    [会话ID Advisor] 使每次对话拥有ID，模型回答不会混乱

--- src/main/java/com/yu/springai/controller/ChatController.java
+++ src/main/java/com/yu/springai/controller/ChatController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
+import static org.springframework.ai.chat.memory.ChatMemory.CONVERSATION_ID;
+
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/ai")
@@ -14,10 +16,11 @@ public class ChatController {
     private final ChatClient chatClient;
 
     @RequestMapping(value = "/chat", produces = "text/html;charset=utf-8")
-    public Flux<String> chat(String prompt) {
+    public Flux<String> chat(String prompt, String chatId) {
         // .call 全部返回  .stream 流式返回
         return chatClient.prompt()
                 .user(prompt)
+                .advisors(a -> a.param(CONVERSATION_ID, chatId))
                 .stream()
                 .content();
     }
