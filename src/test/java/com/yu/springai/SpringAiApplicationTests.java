ba8d0081f96aaaaa30f17ade5c8e6b713186d1aa
yqy <dwadaw@dae>
2025-07-24 17:27:15 +0800

    [Embedding] 引入嵌入向量数据库pipecone，并编写测试用例演示用法

--- src/test/java/com/yu/springai/SpringAiApplicationTests.java
+++ src/test/java/com/yu/springai/SpringAiApplicationTests.java
@@ -2,20 +2,82 @@ package com.yu.springai;
 
 import com.yu.springai.utils.VectorDistanceUtils;
 import org.junit.jupiter.api.Test;
+import org.springframework.ai.document.Document;
 import org.springframework.ai.openai.OpenAiEmbeddingModel;
+import org.springframework.ai.reader.ExtractedTextFormatter;
+import org.springframework.ai.reader.pdf.PagePdfDocumentReader;
+import org.springframework.ai.reader.pdf.config.PdfDocumentReaderConfig;
+import org.springframework.ai.vectorstore.SearchRequest;
+import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
 
 import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 @SpringBootTest
 class SpringAiApplicationTests {
 
     @Autowired
     private OpenAiEmbeddingModel embeddingModel;
+    @Autowired
+    VectorStore vectorStore;
 
+    @Test
+    public void testPDFLoader(){
+        Resource resource = new FileSystemResource("中二知识笔记.pdf");
+        // 1.创建PDF的读取器
+        PagePdfDocumentReader reader = new PagePdfDocumentReader(
+                resource, // 文件源
+                PdfDocumentReaderConfig.builder()
+                        .withPageExtractedTextFormatter(ExtractedTextFormatter.defaults())
+                        .withPagesPerDocument(1) // 每1页PDF作为一个Document
+                        .build()
+        );
+        // 2.读取PDF文档，拆分为Document
+        List<Document> documents = reader.read();
+        // 3.写入向量库
+        documents.forEach(document -> { // 添加元数据
+            document.getMetadata().put("file_name","中二知识笔记.pdf");
+        });
+        vectorStore.add(documents);
+        // 4.搜索
+        SearchRequest request = SearchRequest.builder()
+                .query("论语中教育的目的是什么")
+                .topK(1)
+                .similarityThreshold(0.6)
+                .filterExpression("file_name == '中二知识笔记.pdf'")
+                .build();
+        List<Document> docs = vectorStore.similaritySearch(request);
+        if (docs == null) {
+            System.out.println("没有搜索到任何内容");
+            return;
+        }
+        for (Document doc : docs) {
+            System.out.println(doc.getId());
+            System.out.println(doc.getScore());
+            System.out.println(doc.getText());
+        }
+    }
+
+    @Test
+    public void testVectorStore() {
+        // 存储
+        vectorStore.add(List.of(
+                new org.springframework.ai.document.Document("我想吃炸鸡")
+        ));
+
+        // 查询
+        List<Document> results = vectorStore.similaritySearch(SearchRequest.builder().query("pinecone 配置").topK(3).build());
+        if (results != null && !results.isEmpty()) {
+            results.forEach(doc -> System.out.println(doc.getFormattedContent()));
+        }
+
+    }
 
     @Test
     void contextLoads() {
